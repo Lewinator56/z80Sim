65f3cc8b6a8ddef309639a8d3b36febf146e7d76
lewis dixon <lewis.dixon@live.co.uk>
2021-04-22 20:33:09 +0100

    fixed array indexing added an error popup method

--- z80CpuSim/UI/MainContainer.xaml.cs
+++ z80CpuSim/UI/MainContainer.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Threading;
+using System.IO;
 
 namespace z80CpuSim.UI
 {
@@ -44,6 +45,31 @@ namespace z80CpuSim.UI
         private void openCode_Click(object sender, RoutedEventArgs e)
         {
             CpuControlPanel.EnableControlsForNewExecution();
+            Microsoft.Win32.OpenFileDialog opf = new Microsoft.Win32.OpenFileDialog();
+            opf.Filter = "binary file | *.bin";
+            Nullable<bool> r = opf.ShowDialog();
+            if (r == true)
+            {
+                LoadBin(opf.FileName);
+            }
+        }
+
+        private void LoadBin(string path)
+        {
+            z80CpuSim.CPU.Z80CPU Z80 = z80CpuSim.CPU.Z80CPU.instance();
+            byte[] f = File.ReadAllBytes(path);
+            for (int i = 0; i < f.Length; i++)
+            {
+                try
+                {
+                    Z80.ram.SetAddress((ushort)i, f[i]);
+                } catch
+                {
+                    ErrorPopup ep = new ErrorPopup();
+                    ep.errorText.Text = "The binary file is larger than the maximum memory size of 65535 bytes";
+                    MaterialDesignThemes.Wpf.DialogHost.Show(ep);
+                }
+            }
         }
     }
 }
